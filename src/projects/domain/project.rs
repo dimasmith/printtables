bcbb0d5b4e71ebf75cd88fae22448a2ce22b9ad0
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-10-27 00:52:58 +0300

    style(project): fix code formatting

--- src/projects/domain/project.rs
+++ src/projects/domain/project.rs
@@ -1,6 +1,6 @@
+use async_trait::async_trait;
 use chrono::{Local, NaiveDateTime};
 use uuid::Uuid;
-use async_trait::async_trait;
 
 pub type ProjectId = Uuid;
 
@@ -42,7 +42,6 @@ impl Project {
 /// This trait must be implemented in the infrastructure layer.
 #[async_trait]
 pub trait ProjectRepository: Send + Sync {
-    
     /// Store a new instance of project in storage.
     async fn create(&self, project: Project) -> anyhow::Result<ProjectId>;
 
@@ -50,5 +49,4 @@ pub trait ProjectRepository: Send + Sync {
     /// The implementation must return Ok(None) if the project is not found.
     /// The Err(_) result is for underlying storage communication errors.
     async fn find_by_id(&self, id: ProjectId) -> anyhow::Result<Option<Project>>;
-
 }
