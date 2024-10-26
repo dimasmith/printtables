c3528eb03e661df1d311363c4924097dafa6f060
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-10-27 00:26:48 +0300

    test(project): test project management endpoints

---
+++ tests/register_project.rs
@@ -0,0 +1,75 @@
+use fake::{faker::name::en::Name, Fake};
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use server::start_test_server;
+
+mod server;
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct ProjectView {
+    id: String,
+    name: String,
+}
+
+#[derive(Debug, Serialize)]
+struct RegisterProjectRequest {
+    name: String,
+}
+
+#[tokio::test]
+async fn register_and_view_project() -> anyhow::Result<()> {
+    let test_server = start_test_server().await?;
+
+    let project_request = RegisterProjectRequest::random();
+    let create_project_uri = test_server.uri("/v1/projects");
+
+    let rest_client = reqwest::Client::new();
+    let create_proect_response = rest_client
+        .post(create_project_uri)
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .json(&project_request)
+        .send()
+        .await?;
+
+    assert_eq!(
+        create_proect_response.status(),
+        StatusCode::CREATED,
+        "the service did not respond with created status"
+    );
+
+    let project_location = create_proect_response
+        .headers()
+        .get("location")
+        .ok_or(anyhow::Error::msg("no project ID in location header"))?
+        .to_str()?;
+
+    let get_project_url = test_server.uri(project_location);
+
+    let view_project_response = rest_client
+        .get(get_project_url)
+        .header("Accept", "application/json")
+        .send()
+        .await?;
+
+    assert_eq!(
+        view_project_response.status(),
+        StatusCode::OK,
+        "the newly created project must be found"
+    );
+
+    let project_view = view_project_response.json::<ProjectView>().await?;
+    assert_eq!(
+        &project_view.name, &project_request.name,
+        "project name is not the same as created project name"
+    );
+
+    Ok(())
+}
+
+impl RegisterProjectRequest {
+    fn random() -> Self {
+        let name = Name().fake();
+        Self { name }
+    }
+}
