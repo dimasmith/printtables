ec318850019ab2d26946941378fc6aa6d04a0b77
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-11-24 22:38:51 +0200

    refactor(project): use view model directly in response

--- src/server/routes/projects.rs
+++ src/server/routes/projects.rs
@@ -9,7 +9,6 @@ use uuid::Uuid;
 
 use crate::projects::app::service::ProjectError;
 use crate::projects::domain::name::Name;
-use crate::projects::view::project::ProjectView;
 use crate::projects::{app::service::ProjectsService, domain::project::ProjectId};
 use crate::server::rest::ErrorResponse;
 use crate::shared::validation::validator::CollectingValidator;
@@ -36,19 +35,13 @@ pub async fn register_project(
     }
 }
 
-#[derive(Debug, Clone, Serialize)]
-pub struct ProjectViewPayload {
-    id: ProjectId,
-    name: String,
-}
-
 pub async fn view_project(
     State(project_service): State<Arc<dyn ProjectsService>>,
     Path(project_id): Path<Uuid>,
 ) -> Result<impl IntoResponse, ErrorResponse> {
     let project = project_service.view_project(project_id).await;
     match project {
-        Ok(p) => Ok(Json(ProjectViewPayload::from(p))),
+        Ok(p) => Ok(Json(p)),
         Err(ProjectError::MissingProject) => Err(ErrorResponse::NotFound),
         Err(_) => Err(ErrorResponse::InternalError),
     }
@@ -62,14 +55,6 @@ impl IntoResponse for ProjectCreatedResponse {
             Json(self),
         )
             .into_response()
-    }
-}
-
-impl From<ProjectView> for ProjectViewPayload {
-    fn from(value: ProjectView) -> Self {
-        let id = value.id();
-        let name = value.name().to_string();
-        ProjectViewPayload { id, name }
     }
 }
 
