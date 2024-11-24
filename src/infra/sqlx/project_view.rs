c8beff914938b2b5077553bb2b104b97ced5d957
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-11-25 00:07:58 +0200

    feat(project): display parts bom in project details

--- src/infra/sqlx/project_view.rs
+++ src/infra/sqlx/project_view.rs
@@ -1,14 +1,13 @@
 //! Provides implementation of project view repository.
 
-use anyhow::anyhow;
+use anyhow::bail;
 use async_trait::async_trait;
 use sqlx::SqlitePool;
 use uuid::Uuid;
 
-use crate::projects::{
-    domain::project::ProjectId,
-    view::{project::ProjectView, repository::ProjectViewRepository},
-};
+use crate::projects::domain::project::ProjectId;
+use crate::projects::view::project::ProjectPart;
+use crate::projects::view::{project::ProjectView, repository::ProjectViewRepository};
 
 pub struct SqlxProjectViewRepository {
     pool: SqlitePool,
@@ -23,20 +22,45 @@ impl SqlxProjectViewRepository {
 #[async_trait]
 impl ProjectViewRepository for SqlxProjectViewRepository {
     async fn get_view_by_id(&self, id: ProjectId) -> anyhow::Result<Option<ProjectView>> {
-        let result = sqlx::query!(
+        let project_result = sqlx::query!(
             r#"
-        select id as "id: Uuid", name
-        from project
-        where id = ?
+            select id as "id: Uuid", name 
+            from project
+            where id = ?
         "#,
             id
         )
-        .fetch_one(&self.pool)
+        .fetch_optional(&self.pool)
         .await;
-        match result {
-            Ok(record) => Ok(Some(ProjectView::new(record.id, record.name))),
-            Err(sqlx::Error::RowNotFound) => Ok(None),
-            Err(e) => Err(anyhow!(e)),
-        }
+
+        let project_record = match project_result {
+            Ok(Some(record)) => record,
+            Ok(None) => return Ok(None),
+            Err(e) => bail!(e),
+        };
+
+        let bom_result = sqlx::query!(
+            r#"
+            select bom.part_id as "part_id: Uuid", bom.quantity as "quantity: u32", part.name as part_name 
+            from bom 
+            join part on part.id = bom.part_id
+            where bom.project_id = ?
+        "#,
+            id
+        )
+        .fetch_all(&self.pool)
+        .await;
+
+        let bom_records = match bom_result {
+            Ok(parts) => parts,
+            Err(e) => bail!(e),
+        };
+
+        let parts = bom_records
+            .into_iter()
+            .map(|record| ProjectPart::new(record.part_id, record.part_name, record.quantity))
+            .collect();
+        let project_view = ProjectView::new(project_record.id, project_record.name, parts);
+        Ok(Some(project_view))
     }
 }
